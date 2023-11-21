dd8472eee40856d7b7bdfae67c98e928dc0e114d
Marcos Davi <alietoriando@gmail.com>
2023-11-21 17:44:18 -0300

    [Chore]: Painel do Usuário Landing Page

--- client/scripts/menu-header.js
+++
@@ -1,9 +0,0 @@
-const menuConta = document.getElementById('menu_conta');
-const listaContaUsuario = document.getElementById('menu_conta_usuario');
-
-
-function menuContaClicado(){
-    listaContaUsuario.classList.toggle('menu_clicado');
-}
-
-menuConta.addEventListener('click', () => menuContaClicado())
