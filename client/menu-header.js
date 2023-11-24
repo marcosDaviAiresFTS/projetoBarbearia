b525e4ca1d247d72ccc91325a04278e1005888d5
Marcos Davi <alietoriando@gmail.com>
2023-11-24 14:10:18 -0300

    [Chore]: Atualização de Estilo Landing Page

--- client/menu-header.js
+++ client/menu-header.js
@@ -1,15 +1,15 @@
-const menuConta = document.getElementById('menu_conta');
-const listaContaUsuario = document.getElementById('menu_conta_usuario');
+const menuConta = document.getElementById('user-perfil');
+const listaContaUsuario = document.getElementById('user-panel');
+console.log(listaContaUsuario)
 
 
 function menuContaClicado() {
-    listaContaUsuario.classList.toggle('menu_clicado');
-    if (listaContaUsuario.classList.contains('menu_clicado')){
-        listaContaUsuario.style.padding = '15px 0px'
+    listaContaUsuario.classList.toggle('change-state-user-panel');
+    if (listaContaUsuario.classList.contains('change-state-user-panel')){
     } else {
         setTimeout(() => {
             listaContaUsuario.style.padding = '0px';
-        }, 100);
+        }, 300);
     }
 }
 
