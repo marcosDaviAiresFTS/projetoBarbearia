2cba1d730e0584fe5f554ce6779f7b1f6a43b7c1
Marcos Davi <alietoriando@gmail.com>
2023-12-06 09:18:43 -0300

    [Feature]: Criação da Tela de Serviços

---
+++ client/dist/index.js
@@ -0,0 +1,17 @@
+'use strict';
+function somaDeNumeros(n1, n2) {
+    return n1 + n2;
+}
+const arr = [2, 3, 4, 4, 4, 2, 12, '2'];
+const arr2 = [2, 3, 4, 4, 4, 2, 12, '2'];
+const res = somaDeNumeros(10, 2);
+const obj = {
+    name: 'Marcos',
+    idade: 21,
+    amigos: [
+        { name: 'Breninho', idade: 77, amigos: [] },
+        { name: 'Matheus', idade: 5, amigos: [
+                { name: 'Carlao', idade: 102, amigos: [] }
+            ] }
+    ],
+};
---
+++ client/index.ts
@@ -0,0 +1,66 @@
+// 'use strict';
+
+// function somaDeNumeros(n1: number, n2: number): number {
+//     return n1 + n2;
+// }
+
+// const arr: (number | string)[] = [2, 3, 4, 4, 4, 2, 12, '2'];
+// const arr2: Array<number | string> = [2, 3, 4, 4, 4, 2, 12, '2'];
+// const res: number = somaDeNumeros(10, 2);
+
+// type Pessoa = {
+//     name: string;
+//     idade: number;
+//     amigos: Array<Pessoa>
+// }
+
+// type Agendamento = {
+//     idAgendamento: number,
+//     barbeiro: {
+//         id: number, nome: string
+//     },
+//     usuario?: {
+//         id: number,
+//         nome: string,
+//         email: string,
+//         senha: string
+//     },
+//     servico: Servico,
+//     dia_hora: Date
+// }
+
+// type Servico = {
+//     id: number;
+//     tipo_servico: string;
+//     preco: number;
+// }
+
+// const retornoBackend: Array<Agendamento> = [{}, {}, {}]
+// const servicosBackend: Array<Servico> = [{}, {}, {}]
+
+// const obj: Pessoa = {
+//     name: 'Marcos',
+//     idade: 21,
+//     amigos: [
+//         { name: 'Breninho', idade: 77, amigos: [] },
+//         {
+//             name: 'Matheus', idade: 5, amigos: [
+//                 { name: 'Carlao', idade: 102, amigos: [] }
+//             ]
+//         }
+//     ],
+// }
+
+// type Carro = {
+//     modelo: "Ferrari" | "Porshe";
+//     cor: "Azul" | "Branco";
+//     ano: number;
+// }
+
+// const car: Carro = {
+//     modelo: 'Ferrari',
+//     cor: 'Branco',
+//     ano: 1222
+// }
+
+// const x = document.createElement('button').addEventListener('')
